1c033e29d6f8cac25e4ae6ca029b293b15d27700
rajkumar20197 <rajkumarthota20197@gmail.com>
2025-02-16 13:50:57 -0800

    new account UI

--- Profit_First/src/app/accounts.tsx
+++ Profit_First/src/app/accounts.tsx
@@ -1,8 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native';
-import AccountListItem from '../components/AccountListItem';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 import AccountsList from '../components/AccountsList';
+import Entypo from '@expo/vector-icons/Entypo';
+import React, { useState } from 'react';
+
 
 export default function AccountsScreen() {
+  const [name, setName] = useState('');
+  const [cap, setCap] = useState('');
+  const [tap, setTap] = useState('');
+  
+const createAccount = () => {
+  console.warn('Create Account', name,);
+};
+ 
   return (
     <View style={{ gap: 5, padding: 10 }}>
 
@@ -13,19 +23,42 @@ export default function AccountsScreen() {
       </View>
 
       <AccountsList />
-      
 
-      
+      <View style={styles.inputRow}>
+        <TextInput value={name} onChangeText={setName} placeholder="Name" style={styles.input} />
+        <TextInput value={cap} onChangeText={setCap} placeholder="Cap %" style={styles.input} />
+        <TextInput value={tap} onChangeText={setTap} placeholder="Tap %" style={styles.input} />
+        <Entypo name="check" size={24} color="green" />
+        
+
+        
+        
+      </View>
+
+      <Button title="Add Account" onPress={createAccount}/>
+
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   header: {
-    backgroundColor: 'orange',
+    backgroundColor: 'pink',
     padding: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    
   },
- 
+  inputRow: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+  },
+  input: {
+    flex: 1,
+    // padding: 5,
+    // margin: 5,
+    // backgroundColor: 'lightgrey',
+    // borderRadius: 5,
+  },
 });
